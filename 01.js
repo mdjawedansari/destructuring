669fe358b7517306d91beb5105a62305f00e80b9
jawed0216 <jawed0216@gmail.com>
2023-07-20 19:29:13 +0530

    Done

---
+++ 01.js
@@ -0,0 +1,19 @@
+// Count the occurence
+
+function countWords(str) {
+  const words = str.split(" ");
+  const map = new Map();
+
+  for (let word of words) {
+    if (map.has(word)) {
+      map.set(word, map.get(word) + 1);
+    } else {
+      map.set(word, 1);
+    }
+  }
+
+  return map;
+}
+
+const sentence = "I am learning web development from PW Skills.";
+console.log(countWords(sentence));
---
+++ 05.js
@@ -0,0 +1,11 @@
+// Min and max values.
+
+function maxMin(arr) {
+  return {
+    max: Math.max(...arr),
+    min: Math.min(...arr),
+  };
+}
+
+const array = [3, 7, 2, 9, 4, 1];
+console.log(maxMin(array));
